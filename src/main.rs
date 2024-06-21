4728d078ca4a994274ee574545ea0b01ad23d733
Christophe Thibaut <cthibauttof@gmail.com>
2024-06-21 15:17:02 +0200

    episode two : drawing area on window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::f64::consts::PI;
+use gtk::cairo::Context;
+use gtk::DrawingArea;
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow};
 
@@ -14,12 +17,65 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 
+fn draw_it(drawing_area: &DrawingArea, context: &Context, _width: i32, _height: i32) {
+    let allocation = drawing_area.allocation();
+    let width = allocation.width() as f64;
+    let height = allocation.height() as f64;
+    let lesser = width.min(height);
+
+    // Coordinates for the center of the window
+    let xc = width / 2.0;
+    let yc = height / 2.0;
+
+    context.set_line_width(lesser * 0.02); // outline thickness changes with window size
+
+    // First draw a simple unclosed arc
+    let _ = context.save();
+    context.arc(width / 3.0, height / 4.0, lesser / 4.0, -(PI / 5.0), PI);
+    context.close_path(); // line back to start point
+    context.set_source_rgb(0.0, 0.8, 0.0);
+    let _ = context.fill_preserve();
+    let _ = context.restore(); // back to opaque black
+    let _ = context.stroke();
+
+    // Now draw a circle
+    let _ = context.save();
+    context.arc(xc, yc, lesser / 4.0, 0.0, 2.0 * PI); // full circle
+    context.set_source_rgba(0.0, 0.0, 0.8, 0.6); // partially translucent
+    let _ = context.fill_preserve();
+    let _ = context.restore(); // back to opaque black
+    let _ = context.stroke();
+
+    // And finally an ellipse
+    let ex = xc;
+    let ey = 3.0 * height / 4.0;
+    let ew = 3.0 * width / 4.0;
+    let eh = height / 3.0;
+
+    let _ = context.save();
+    context.translate(ex, ey); // make (ex, ey) == (0, 0)
+    context.scale(ew / 2.0, eh / 2.0); // for width: ew / 2.0 == 1.0 for height: eh / 2.0 == 1.0
+    context.arc(0.0, 0.0, 1.0, 0.0, 2.0 * PI); // 'circle' centered at (0, 0) with 'radius' of 1.0
+    context.set_source_rgba(0.8, 0.0, 0.0, 0.7);
+    let _ = context.fill_preserve();
+    let _ = context.restore(); // back to opaque black
+    let _ = context.stroke();
+}
+
 fn build_ui(app: &Application) {
     // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("Episode One: Display a Window")
+        .title("Episode Two: Draw on the Window")
         .build();
+
+    let drawing_area = DrawingArea::new();
+    drawing_area.set_content_width(1000);
+    drawing_area.set_content_height(500);
+    drawing_area.set_draw_func(draw_it);
+
+    window.set_child(Some(&drawing_area));
+
 
     // Present window
     window.present();
